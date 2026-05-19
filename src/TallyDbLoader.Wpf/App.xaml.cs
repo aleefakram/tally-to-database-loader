f9d9ae5c16e85b9b51afaffb2ec4be6e82315d16
aleefakram <aleefakramm@gmail.com>
2026-05-19 14:07:11 +0530

    feat: implement single-instance process protection using a System Mutex

--- src/TallyDbLoader.Wpf/App.xaml.cs
+++ src/TallyDbLoader.Wpf/App.xaml.cs
@@ -1,13 +1,42 @@
-﻿using System.Configuration;
-using System.Data;
+using System;
+using System.Threading;
 using System.Windows;
 
-namespace TallyDbLoader.Wpf;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace TallyDbLoader.Wpf
 {
-}
+    public partial class App : Application
+    {
+        private const string AppMutexName = "Global\\TallyToDbLoaderMutex_662bd342-d285-4831-a40d";
+        private static Mutex? _mutex;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _mutex = new Mutex(true, AppMutexName, out bool isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("Another instance of Tally-to-Database Sync is already running.", 
+                                "Already Running", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                try
+                {
+                    _mutex.ReleaseMutex();
+                }
+                catch (ObjectDisposedException) { }
+                catch (ApplicationException) { }
+                _mutex.Dispose();
+            }
+            base.OnExit(e);
+        }
+    }
+}
